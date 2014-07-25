e7047a4e35aed68fcca6013ffb969e0bf9fed2d4
Yen-Jie Lee <yenjie@mit.edu>
2014-07-25 14:12:11 +0200

    cosmetic update

--- cmssw/JewelInterface/src/JewelHadronizer.cc
+++ cmssw/JewelInterface/src/JewelHadronizer.cc
@@ -69,63 +69,26 @@ JewelHadronizer::~JewelHadronizer()
 void JewelHadronizer::add_heavy_ion_rec(HepMC::GenEvent *evt)
 {
   // heavy ion record in the final CMSSW Event
+  // Yen-Jie: set to 0 for the moment
   HepMC::HeavyIon* hi = new HepMC::HeavyIon(
     0,                               // Ncoll_hard/N of SubEvents
     0,                               // Npart_proj
     0,                               // Npart_targ
-    0, // Ncoll
-    0,                                   // spectator_neutrons
-    0,                                   // spectator_protons
-    0,                          // N_Nwounded_collisions
-    0,                          // Nwounded_N_collisions
-    0,                          // Nwounded_Nwounded_collisions
-    //gsfs Changed from 19 to 18 (Fortran counts from 1 , not 0) 
-    0,                   // impact_parameter in [fm]
+    0,                               // Ncoll
+    0,                               // spectator_neutrons
+    0,                               // spectator_protons
+    0,                               // N_Nwounded_collisions
+    0,                               // Nwounded_N_collisions
+    0,                               // Nwounded_Nwounded_collisions
+                                     //gsfs Changed from 19 to 18 (Fortran counts from 1 , not 0) 
+    0,                               // impact_parameter in [fm]
     0,                               // event_plane_angle
-    0,                                   // eccentricity
-    //gsfs Changed from 12 to 11 (Fortran counts from 1 , not 0) 
-    0                    // sigma_inel_NN
+    0,                               // eccentricity
+                                     //gsfs Changed from 12 to 11 (Fortran counts from 1 , not 0) 
+    0                                // sigma_inel_NN
   );
   evt->set_heavy_ion(*hi);
   delete hi;
-}
-
-//___________________________________________________________________     
-HepMC::GenParticle* JewelHadronizer::build_hijing(int index, int barcode)
-{
-   // Build particle object corresponding to index in hijing
-
-/*                                                                                                                                                         
-   double x0 = himain2.patt[0][index];
-   double y0 = himain2.patt[1][index];
-
-   double x = x0*cosphi0_-y0*sinphi0_;
-   double y = y0*cosphi0_+x0*sinphi0_;
-
-   // Jewel gives V0's status=4, they need to have status=1 to be decayed in geant
-   // also change status=11 to status=2
-   if(himain2.katt[3][index]<=10 && himain2.katt[3][index]>0) himain2.katt[3][index]=1;
-   if(himain2.katt[3][index]<=20 && himain2.katt[3][index]>10) himain2.katt[3][index]=2;
-
-   HepMC::GenParticle* p = new HepMC::GenParticle(
-                                                  HepMC::FourVector(x,  // px                                                                            
-                                                                    y,  // py                                                                            
-                                                                    himain2.patt[2][index],  // pz                                                         
-                                                                    himain2.patt[3][index]), // E                                                          
-                                                  himain2.katt[0][index],// id                                                                             
-                                                  himain2.katt[3][index] // status                                                          
-                                                  );
-   p->suggest_barcode(barcode);
-   
-   return p;
-*/
-   return 0;
-}
-
-//___________________________________________________________________     
-HepMC::GenVertex* JewelHadronizer::build_hijing_vertex(int i,int id)
-{
-   return 0;
 }
 
 bool JewelHadronizer::generatePartonsAndHadronize()
@@ -134,15 +97,13 @@ bool JewelHadronizer::generatePartonsAndHadronize()
    static int j=0;
    j++;
    GENEVENT(j);
-   call_pyhepc(1);
-   
+   cout <<pysubs_.ckin[2]<<endl;
+   call_pyhepc(1);   
    HepMC::GenEvent* evt = hepevtio.read_next_event();
 
    evt->set_signal_process_id(pypars.msti[0]);	 // type of the process
    evt->set_event_scale(pypars.pari[16]);  	 // Q^2
 
-   //if(embedding_) rotateEvtPlane(evt,evtPlane_);
-   //add_heavy_ion_rec(evt);
    event().reset(evt);
    return true;
 }
@@ -150,6 +111,7 @@ bool JewelHadronizer::generatePartonsAndHadronize()
 //_____________________________________________________________________  
 bool JewelHadronizer::get_particles(HepMC::GenEvent *evt )
 {      
+   return 0;
 }
 
 //_____________________________________________________________________
@@ -162,20 +124,8 @@ bool JewelHadronizer::call_hijset(double efrm, std::string frame, std::string pr
 
 //______________________________________________________________
 bool JewelHadronizer::initializeForInternalPartons(){
-
-  //initialize pythia5
-
-//  if(0){
-//    std::string dumstr = "";
-//    call_pygive(dumstr);
-//  }
-
-   // initialize hijing
-//   LogInfo("HIJINGinAction") << "##### Calling HIJSET(" << efrm_ << "," <<frame_<<","<<proj_<<","<<targ_<<","<<iap_<<","<<izp_<<","<<iat_<<","<<izt_<<") ####";
-//   call_hijset(efrm_,frame_,proj_,targ_,iap_,izp_,iat_,izt_);
-
+   call_hijset(efrm_,frame_,proj_,targ_,iap_,izp_,iat_,izt_);
    return true;
-
 }
 
 bool JewelHadronizer::declareStableParticles( std::vector<int> pdg )
