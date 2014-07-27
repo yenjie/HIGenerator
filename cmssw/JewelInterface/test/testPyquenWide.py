baa2b49c119da29172222b4796d101c016994053
Yen-Jie Lee <yenjie@mit.edu>
2014-07-27 21:58:53 +0200

    add PYTHIA and PYQUEN test configuration

--- cmssw/JewelInterface/test/testPyquenWide.py
+++ cmssw/JewelInterface/test/testPyquenWide.py
@@ -12,24 +12,18 @@ process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(100000)
 
 process.source = cms.Source("EmptySource")
 
-process.load("Configuration.Generator.PythiaUEZ2starSettings_cfi");
-process.generator = cms.EDFilter("Pythia6GeneratorFilter",
-    pythiaHepMCVerbosity = cms.untracked.bool(False),
-    maxEventsToPrint = cms.untracked.int32(0),
-    pythiaPylistVerbosity = cms.untracked.int32(0),
-    filterEfficiency = cms.untracked.double(1.0),
-    comEnergy = cms.double(2760.0),
-    PythiaParameters = cms.PSet(
-        process.pythiaUESettingsBlock,
-        processParameters = cms.vstring('MSEL=1               ! QCD hight pT processes', 
-            'CKIN(3)=80.          ! minimum pt hat for hard interactions', 
-            'CKIN(4)=9990.         ! maximum pt hat for hard interactions'),
-        # This is a vector of ParameterSet names to be read, in this order
-        parameterSets = cms.vstring('pythiaUESettings', 
-            'processParameters')
-    )
-)
+process.load("GeneratorInterface.PyquenInterface.pyquenDefault_cfi")
 
+process.generator.doQuench = True
+process.generator.doRadiativeEnLoss = True
+process.generator.doCollisionalEnLoss = False
+process.generator.qgpInitialTemperature = 1
+process.generator.angularSpectrumSelector = 1
+
+process.generator.doIsospin = cms.bool(False)
+process.generator.comEnergy =cms.double(2760)
+process.generator.PythiaParameters.parameterSets = cms.vstring('pythiaUESettings','ppJets','kinematics')
+process.generator.PythiaParameters.kinematics = cms.vstring('CKIN(3) = 100','CKIN(4) = 9999')
 
 process.RandomNumberGeneratorService.generator.initialSeed = 5
 
@@ -44,7 +38,7 @@ process.ana = cms.EDAnalyzer('HydjetAnalyzer'
 process.dijet = cms.EDAnalyzer('DijetNtupleProducer')
 
 process.TFileService = cms.Service('TFileService',
-                                   fileName = cms.string('PythiaZ2Star.root')
+                                   fileName = cms.string('PyquenWide.root')
                                    )
 
 
